84805cd05c048a900befad62fe34c5b227994a88
tomer golan <tomer golan>
2016-05-24 10:02:09 +0300

    added "{user} is typing" functionality

--- server.js
+++ server.js
@@ -2,19 +2,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var system = "system";
+var clients = {};
+var system = "*** system ***";
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
+  clients[socket.id] = socket;
+
+  socket.emit('save id', socket.id);
+
   socket.broadcast.emit('chat message', system, 'someone connected');
+
   socket.on('disconnect', function(){
     io.emit('chat message', system, 'someone disconnect');
   });
-  socket.on('chat message', function(nickname, msg){
-    socket.broadcast.emit('chat message', nickname, msg);
+
+  socket.on('chat message', function(user, msg){
+    socket.broadcast.emit('chat message', user, msg);
+  });
+
+  socket.on('user typing', function(user){
+    socket.broadcast.emit('user typing', socket.id, user);
+  });
+
+  socket.on('end typing', function(user){
+    socket.broadcast.emit('end typing', socket.id, user);
   });
 });
 
