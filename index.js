524745d44848991740edbc67a932e290c9ba84f8
tomer golan <tomer golan>
2016-05-25 09:26:50 +0300

    added public dir

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var clients = {};
 var system = "*** system ***";
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
---
+++ public/scripts/chat.js
@@ -0,0 +1,48 @@
+  "use strict";
+  var socket = io();
+  var sid = null;
+  $('form').submit(function(){
+    var user = ($('#user').val() || "user");
+    var msg = $('#m').val();
+    appendMessage(user, msg);
+    socket.emit('chat message', user, msg);
+    socket.emit('end typing', user);
+    $('#m').val('');
+    return false;
+  });
+
+  $('#m').keydown(function(){
+    var user = ($('#user').val() || "user");
+    socket.emit('user typing', user);
+  });
+
+  socket.on('save id', function(sid_){
+    sid = sid_;
+  });
+
+  socket.on('chat message', function(user, msg){
+    appendMessage(user, msg);
+  });
+
+  socket.on('user typing', function(sid, user){
+    appendTyping(sid, user);
+  });
+
+  socket.on('end typing', function(sid, user){
+    removeTyping(sid, user);
+  });
+
+  function appendMessage(user, msg){
+    $('#messages').append($('<li>').text(user+ ": " + msg));
+  };
+
+  function appendTyping(sid, user){
+    removeTyping(sid, user);
+    var e = $('<li>').text(user + " is typing...");
+    $('#typings').append(e);
+    e.attr('id', user);
+  };
+
+  function removeTyping(sid, user){
+    $('#'+user).remove();
+  };
