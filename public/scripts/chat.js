58ae5968c18150ca8489fdaa2e021e6a99e347bf
tomer golan <tomer golan>
2016-05-26 10:12:24 +0300

    angular implemntation in progress

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,48 +1,75 @@
   "use strict";
-  var socket = io();
-  var sid = null;
-  $('form').submit(function(){
-    var user = ($('#user').val() || "user");
-    var msg = $('#m').val();
-    appendMessage(user, msg);
-    socket.emit('chat message', user, msg);
-    socket.emit('end typing', user);
-    $('#m').val('');
-    return false;
-  });
 
-  $('#m').keydown(function(){
-    var user = ($('#user').val() || "user");
-    socket.emit('user typing', user);
-  });
+var module = angular.module("chatApp",[]);
 
-  socket.on('save id', function(sid_){
-    sid = sid_;
-  });
+var socket = io();
 
-  socket.on('chat message', function(user, msg){
-    appendMessage(user, msg);
-  });
+module.controller("mainCtrl",
+  ['$scope',function mainCtrl($scope){
+    $scope.model = {
+      online: [], //{user: "tomer", typing: true}
+      messages: [], //{user: "tomer", message: "hello"}
+      sid: null,
+      user: null,
+      message: null
+    }
 
-  socket.on('user typing', function(sid, user){
-    appendTyping(sid, user);
-  });
+    $scope.send = function (){
+      $scope.model.messages.push({user: $scope.model.user, message: $scope.model.message});
+      socket.emit('chat message', $scope.model.user, $scope.model.message);
+      socket.emit('end typing', $scope.model.user);
+      $scope.model.message = "";
+    };
 
-  socket.on('end typing', function(sid, user){
-    removeTyping(sid, user);
-  });
+    socket.on('chat message', function(user_, msg_){
+      $scope.model.messages.push({user: user_, message: msg_});
+    });
+}]);
 
-  function appendMessage(user, msg){
-    $('#messages').append($('<li>').text(user+ ": " + msg));
-  };
-
-  function appendTyping(sid, user){
-    removeTyping(sid, user);
-    var e = $('<li>').text(user + " is typing...");
-    $('#typings').append(e);
-    e.attr('id', user);
-  };
-
-  function removeTyping(sid, user){
-    $('li#'+user).remove();
-  };
+  //
+  // var sid = null;
+  // $('form').submit(function(){
+  //   var user = ($('#user').val() || "user");
+  //   var msg = $('#m').val();
+  //   appendMessage(user, msg);
+  //   socket.emit('chat message', user, msg);
+  //   socket.emit('end typing', user);
+  //   $('#m').val('');
+  //   return false;
+  // });
+  //
+  // $('#m').keydown(function(){
+  //   var user = ($('#user').val() || "user");
+  //   socket.emit('user typing', user);
+  // });
+  //
+  // socket.on('save id', function(sid_){
+  //   sid = sid_;
+  // });
+  //
+  // socket.on('chat message', function(user, msg){
+  //   appendMessage(user, msg);
+  // });
+  //
+  // socket.on('user typing', function(sid, user){
+  //   appendTyping(sid, user);
+  // });
+  //
+  // socket.on('end typing', function(sid, user){
+  //   removeTyping(sid, user);
+  // });
+  //
+  // function appendMessage(user, msg){
+  //   $('#messages').append($('<li>').text(user+ ": " + msg));
+  // };
+  //
+  // function appendTyping(sid, user){
+  //   removeTyping(sid, user);
+  //   var e = $('<li>').text(user + " is typing...");
+  //   $('#typings').append(e);
+  //   e.attr('id', user);
+  // };
+  //
+  // function removeTyping(sid, user){
+  //   $('li#'+user).remove();
+  // };
